60e3da2429c2b3c474ce3b69499ad892be4e157a
Eugene Javiñas <eugenejavinas@gmail.com>
2026-06-24 20:50:00 +0800

    Fix window insets

--- app/src/main/java/com/yudzeen/gweatherexercise/ui/MainActivity.kt
+++ app/src/main/java/com/yudzeen/gweatherexercise/ui/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.yudzeen.gweatherexercise.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.yudzeen.gweatherexercise.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,5 +15,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val rootView = findViewById<View>(android.R.id.content)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
